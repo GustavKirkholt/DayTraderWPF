b8ee805bc5dd0c39f31bce8e8d8e8af52351eba1
Gustav Kirkholt <s164765@win.dtu.dk>
2022-11-21 13:53:46 +0100

    Added BankAccount model

---
+++ DayTraderWPF/Models/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTraderWPF.Models
+{
+    public class BankAccount
+    {
+        protected decimal balance;
+        public BankAccount(decimal b)
+        {
+            balance = b;
+        }
+
+        public BankAccount() :
+          this(0.0M)
+        {
+        }
+
+        public virtual decimal Balance
+        {
+            get { return balance; }
+        }
+
+        public virtual void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public virtual void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public override string ToString()
+        {
+            return "account holds " +
+                  +balance + " kroner";
+        }
+    }
+}
