5b35b05c85481ab6f5e2a122a0977c4337df67c6
RezaHadd <56150703+RezaHadd@users.noreply.github.com>
2022-11-22 11:36:57 +0100

    Testing git commit/push with a prop class (Algorithms.cs) Suspecting minor bugs with git. Ignore this commit.

---
+++ DayTraderWPF/Models/Algorithms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTraderWPF.Models
+{
+    internal class Algorithms
+    {
+        //TODO Implement different algorithmic trading strategies. Consider one class per algo under a new folder.
+    }
+}
